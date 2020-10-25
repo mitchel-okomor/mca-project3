264b515d60f5dd249f492e783691106fdf1bb0eb
CodeXaver <mitchel.okomor@gmail.com>
2020-10-25 23:56:17 +0100

    changed nav-link colors

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleDisplay (id){
+   var el = document.getElementById(id);
+   if(el.classList.contains("hide")){
+       console.log("contained");
+       el.classList.add("show")
+       el.classList.remove("hide");
+   }else{
+      el.classList.add("hide");
+      el.classList.remove("show")
+}
+
+}
